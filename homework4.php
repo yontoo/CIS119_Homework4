f928cb143a506fc1659ae9f4ea94b2451ed944ab
yontoo <traylee1513@yahoo.com>
2019-06-25 17:47:57 -0700

    Add files via upload

--- homework4.php
+++ homework4.php
@@ -1,96 +1,67 @@
 <!-- Just as a precursor, some of the functions don't work without a valid API key inside of a file called "apikey.php". -->
 <?php
-     
-        // $baseImageURL = 'http://ddragon.leagueoflegends.com/cdn/9.12.1/img/champion/'; 
-        // $image = $baseImageURL.$champion.'_'.$number.'.png';
-        class Summoner
-        {
-            public $name;
-            public $level;
-            private $summ_id;
-            private $json_link;
-            private $key;
-            public $json;
+    include "summoner.php";
 
+    $summoner_test = new Summoner($_POST["summoner"]);
+    // echo "<h3>Summoner: ".$summoner_test->name."</h3><br>";
+    // echo "<img src=".$summoner_test->getChampIcon($summoner_test->mastery_info[0]->championId)." alt=\"ChampionIcon\">";
+    // echo "<br> Mastery Points: ".number_format($summoner_test->mastery_info[0]->championPoints, 0,'.',',');
 
-            public function getJson($json_link)
-            {
-                $this->json_contents=file_get_contents($json_link);
-                // $this->json
-                return json_decode($this->json_contents);
-            }
-
-            public function getChampIcon($champ_id)
-            {
-                $champ_array = $this->getChamps();
-                return "http://ddragon.leagueoflegends.com/cdn/".$this->curr_ver."/img/champion/".$champ_array[$champ_id].".png";
-            }
-
-            public function getId()
-            {
-                return $this->summ_id;
-            }
-
-            private function getKey()
-            {
-                include "apikey.php";
-                return $key;
-            }
-
-            public function getChamps()
-            {
-                //TODO: Get this function working. Populate an array with champion info, using their ID as the key and their name as the value.
-                $this->champ_info = $this->getJson("http://ddragon.leagueoflegends.com/cdn/".$this->curr_ver."/data/en_US/champion.json");
-                $champions = [];
-                foreach($this->champ_info->data as $x)
-                {
-                    $champions[$x->key] = $x->id;
-                }
-                //For debugging.
-                // echo "<pre>";
-                // print_r($champions);
-                // echo "</pre>";
-                return $champions;
-            }
-
-            function __construct($name_in)
-            {
-                $this->key = $this->getKey();
-                $this->summ_info = $this->getJson("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/".$name_in."?api_key=".$this->key);
-                $this->name = $this->summ_info->name;
-                $this->summ_id = $this->summ_info->id;
-                $this->level = $this->summ_info->summonerLevel;
-                $this->mastery_info = $this->getMastery();
-                $this->curr_ver = $this->getJson("https://ddragon.leagueoflegends.com/api/versions.json")[0];
-            }
-            private function getMastery()
-            {
-                return $this->getJson("https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/".$this->summ_id."?api_key=".$this->key);
-            }
-        }
-
-
-        $summoner_test = new Summoner("GerbilStore");
-        echo "Summoner: ".$summoner_test->name."<br>";
-        echo "<img src=".$summoner_test->getChampIcon($summoner_test->mastery_info[0]->championId)." alt=\"Rengar\">";
-        echo "<br> Mastery Points: ".$summoner_test->mastery_info[0]->championPoints;
-        
-
-        // echo "<br>".$summoner_test->getId();
-        // echo "<br>".$summoner_test->level."<br>";
-        // var_dump("<pre>");
-        // var_dump($summoner_test);
+    // echo "<br>".$summoner_test->getId();
+    // echo "<br>".$summoner_test->level."<br>";
+    // var_dump("<pre>");
+    // var_dump($summoner_test);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
         <title>
-            Homework 4
+            <?php echo $summoner_test->name; ?>
         </title>
     </head>
     <body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a class="navbar-brand text-white">Summoner Search</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation"></button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="homework4-form.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="#">Action 1</a>
+                        <a class="dropdown-item" href="#">Action 2</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0" action="homework4.php" method="post">
+                <input class="form-control mr-sm-2" type="text" placeholder="Summoner" name="summoner">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <div>
+    <?php
+        // echo $_POST["summoner"]
+        // $summoner_test = new Summoner($_POST["summoner"]);
+        echo "<h3>Summoner: ".$summoner_test->name."</h3><br>";
+        echo "<img src=".$summoner_test->getChampIcon($summoner_test->mastery_info[0]->championId)." alt=\"ChampionIcon\">";
+        echo "<br> Mastery Points: ".number_format($summoner_test->mastery_info[0]->championPoints, 0,'.',',');
+    ?>
+    </div>
 
     </body>
 </html>
